12fe18685e99fc2594c1adab1bf49ba2e9d11c17
gargiscode <gargi.pattanayak@gmail.com>
2021-11-07 23:02:51 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DataStructuresWithStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Given a string S, find the longest palindromic substring in S.
+            //Input: "babad"
+            //Output: "bab"
+            string inputStr = "babad";
+            string maxSub = string.Empty;
+            int i, j;
+            int maxLength = 1;
+            
+
+            for (i = 0; i < inputStr.Length; i++)
+            { 
+                for (j = i; j < inputStr.Length; j++)
+                {
+                    if (inputStr[i] == inputStr[j])
+                    { 
+                        
+                    }
+                }
+            }
+
+
+
+
+        
+        }
+    }
+}
